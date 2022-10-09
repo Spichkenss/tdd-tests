65c665610647f8e6989f03d7bdaabfe3a5ded137
JessLap <jesslap@bk.ru>
2022-10-09 16:37:29 +0300

    auth - successfull registration test created

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,17 +1,47 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import {JwtService} from "@nestjs/jwt";
+import {Test} from "@nestjs/testing";
+import {getRepositoryToken} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {UserEntity} from "../user/user.entity";
+import {UserService} from "../user/user.service";
+import {AuthController} from "./auth.controller";
+import {AuthService} from "./auth.service";
+import {RegisterDto} from "./dto/auth.dto";
+
 
 describe('AuthController', () => {
-  let controller: AuthController;
+    let authController: AuthController
+    let authService: AuthService
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
-    }).compile();
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [],
+            controllers: [AuthController],
+            providers: [AuthService, UserService, JwtService, {provide: getRepositoryToken(UserEntity), useValue: Repository}]
+        }).compile()
 
-    controller = module.get<AuthController>(AuthController);
-  });
+        authController = moduleRef.get<AuthController>(AuthController)
+        authService = moduleRef.get<AuthService>(AuthService)
 
-});
+        jest.clearAllMocks()
+    })
+
+    describe('registration', () => {
+        let response
+        let dto: RegisterDto = {email: 'test@mail.ru', password: '12345', name: 'Alexander', surname: 'Blinov'}
+
+        test('successful registration', async () => {
+            response = {user: {id: expect.any(Number), email: dto.email,}, accessToken: expect.any(String)}
+            jest.spyOn(authService, 'register').mockReturnValue(response)
+            const data = await authController.register(dto)
+            expect(authService.register).toBeCalledWith(dto)
+            expect(data).toEqual(response)
+        })
+
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+})
