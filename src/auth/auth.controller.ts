17f02245cd8b05801e2a0d4425b2fb46d8a30576
JessLap <jesslap@bk.ru>
2022-10-09 16:46:51 +0300

    auth - registration function created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {BadRequestException, Body, Controller, HttpCode, Post, UsePipes, ValidationPipe} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import {RegisterDto} from "./dto/auth.dto";
+import {AuthResponse} from "./types/auth.types";
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('register')
+  async register(@Body() dto: RegisterDto): Promise<BadRequestException | AuthResponse>
+  {
+    return this.authService.register(dto)
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from "@nestjs/jwt";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { UserEntity } from "../user/user.entity";
+import { RegisterDto } from './dto/auth.dto';
+import {compare, genSalt, hash} from "bcryptjs"
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(@InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+        private readonly jwtService: JwtService) { }
+
+    async register(dto: RegisterDto) {
+        const isUserExists = await this.userRepository.findOneBy({ email: dto.email })
+        if (isUserExists) throw new BadRequestException("Пользователь с таким email уже зарегестрирован!")
+
+        const salt = await genSalt(10)
+
+        const newUser = await this.userRepository.create({
+            email: dto.email,
+            name: dto.name,
+            surname: dto.surname,
+            password: await hash(dto.password, salt)
+        })
+
+        const user = await this.userRepository.save(newUser)
+
+        return {
+            user: this.returnUserFields(user),
+            accessToken: await this.createAccesToken(user.id)
+        }
+    }
+
+    async createAccesToken(userId: number) {
+        const data = {
+            id: userId
+        }
+        return await this.jwtService.signAsync(data, { expiresIn: '31d' })
+    }
+
+    returnUserFields(user: UserEntity) {
+        return {
+            id: user.id,
+            email: user.email
+        }
+    }
+
+}
