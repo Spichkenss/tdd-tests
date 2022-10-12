d4eac9d0d3bd4ca11c79e4d901cfad5f294a6923
Spichkenss <spichkens.2002@mail.ru>
2022-10-12 14:07:24 +0300

    post - create post function created

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {CreatePostDto} from "./dto/post.dto";
 import { PostService } from './post.service';
+import {CreatePostReponse} from "./types/post.types";
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+
+  @Post('create')
+  create(@Body() createPostDto: CreatePostDto): Promise<CreatePostReponse> {
+    return this.postService.create(createPostDto)
+  }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {CreatePostDto} from "./dto/post.dto";
+import {PostEntity} from "./post.entity";
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(@InjectRepository(PostEntity)
+                private readonly  postRepository: Repository<PostEntity>) {}
+
+    async create(createPostDto: CreatePostDto){
+        const newPost = await this.postRepository.create({
+            text: createPostDto.text
+        })
+
+        const post = await this.postRepository.save(newPost)
+
+        return {
+            post: this.returnPostFields(post),
+            message: 'Пост создан успешно'
+        }
+    }
+
+    returnPostFields(post: PostEntity){
+        return {
+            id: post.id,
+            text: post.text,
+            likes_counter: post.likes_counter,
+            comments_counter: post.comments_counter
+        }
+    }
+}
